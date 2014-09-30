2971fb92fd46d774166e63b9c8fca0ad0ac99602
Ido Rosen <code@idorosen.com>
2014-09-29 21:45:04 -0400

    bash-92.1.2 (aka OSX bash Update 1.0) from http://opensource.apple.com/tarballs/bash/bash-92.1.2.tar.gz

--- bash-3.2/variables.c
+++ bash-3.2/variables.c
@@ -70,14 +70,17 @@
 #  include "pcomplete.h"
 #endif
 
+#include <syslog.h>
+
 #define TEMPENV_HASH_BUCKETS	4	/* must be power of two */
 
 #define ifsname(s)	((s)[0] == 'I' && (s)[1] == 'F' && (s)[2] == 'S' && (s)[3] == '\0')
 
-#define BASHFUNC_PREFIX		"BASH_FUNC_"
-#define BASHFUNC_PREFLEN	10	/* == strlen(BASHFUNC_PREFIX */
-#define BASHFUNC_SUFFIX		"%%"
-#define BASHFUNC_SUFFLEN	2	/* == strlen(BASHFUNC_SUFFIX) */
+/* Wrap bash function exports/imports to avoid hijacking of typical USER_AGENT and similar */
+#define ENV_FUNCTION_PREFIX     "__BASH_FUNC<"
+#define ENV_FUNCTION_PREFIX_LEN (sizeof(ENV_FUNCTION_PREFIX)-1)
+#define ENV_FUNCTION_SUFFIX     ">()"
+#define ENV_FUNCTION_SUFFIX_LEN (sizeof(ENV_FUNCTION_SUFFIX)-1)
 
 extern char **environ;
 
@@ -314,41 +317,40 @@ initialize_shell_variables (env, privmode)
 
       /* If exported function, define it now.  Don't import functions from
 	 the environment in privileged mode. */
-      if (privmode == 0 && read_but_dont_execute == 0 &&
-	  STREQN (BASHFUNC_PREFIX, name, BASHFUNC_PREFLEN) &&
-	  STREQ (BASHFUNC_SUFFIX, name + char_index - BASHFUNC_SUFFLEN) &&
-	  STREQN ("() {", string, 4))
+      int legacy_function = 0; /* 1 just to syslog ignored function names */
+      if (privmode == 0 && read_but_dont_execute == 0 && (legacy_function=STREQN ("() {", string, 4)) &&
+          (char_index > ENV_FUNCTION_SUFFIX_LEN) &&
+          STREQN(ENV_FUNCTION_PREFIX, name, ENV_FUNCTION_PREFIX_LEN) &&
+          STREQ(name + char_index - ENV_FUNCTION_SUFFIX_LEN, ENV_FUNCTION_SUFFIX))
 	{
-	  size_t namelen;
-	  char *tname;		/* desired imported function name */
+      char *temp_name = name + ENV_FUNCTION_PREFIX_LEN;
+      /* Undone later */
+      name[char_index - ENV_FUNCTION_SUFFIX_LEN] = '\0';
+      int temp_name_length = strlen(temp_name);
+      string_length = strlen (string);
+      int temp_string_len = 2 + string_length + temp_name_length;
+        
+	  temp_string = (char *)xmalloc (temp_string_len);
 
-	  namelen = char_index - BASHFUNC_PREFLEN - BASHFUNC_SUFFLEN;
-
-	  tname = name + BASHFUNC_PREFLEN;	/* start of func name */
-	  tname[namelen] = '\0';		/* now tname == func name */
-
-	  string_length = strlen (string);
-	  temp_string = (char *)xmalloc (namelen + string_length + 2);
-
-	  memcpy (temp_string, tname, namelen);
-	  temp_string[namelen] = ' ';
-	  memcpy (temp_string + namelen + 1, string, string_length + 1);
+      snprintf(temp_string, temp_string_len, "%s %s", temp_name, string);
 
 	  /* Don't import function names that are invalid identifiers from the
 	     environment. */
-	  if (absolute_program (tname) == 0 && (posixly_correct == 0 || legal_identifier (tname)))
-	    parse_and_execute (temp_string, tname, SEVAL_NONINT|SEVAL_NOHIST|SEVAL_FUNCDEF|SEVAL_ONECMD);
+	  if (legal_identifier (temp_name))
+	    parse_and_execute (temp_string, temp_name, SEVAL_NONINT|SEVAL_NOHIST|SEVAL_FUNCDEF|SEVAL_ONECMD);
 
-	  if (temp_var = find_function (tname))
+	  if (temp_var = find_function (temp_name))
 	    {
 	      VSETATTR (temp_var, (att_exported|att_imported));
 	      array_needs_making = 1;
 	    }
-	  else
-	    report_error (_("error importing function definition for `%s'"), tname);
-
-	  /* Restore original suffix */
-	  tname[namelen] = BASHFUNC_SUFFIX[0];
+      else {
+        syslog(LOG_ERR, "CVE-2014-6271 invalid function definition");
+	    report_error (_("error importing function definition for `%s'"), temp_name);
+      }
+        // restore original
+        name[char_index - ENV_FUNCTION_SUFFIX_LEN] = ENV_FUNCTION_SUFFIX[0];
+        legacy_function = 0;
 	}
 #if defined (ARRAY_VARS)
 #  if 0
@@ -371,6 +373,9 @@ initialize_shell_variables (env, privmode)
 	  array_needs_making = 1;
 	}
 
+      if (legacy_function) {
+          syslog(LOG_INFO, "CVE-2014-6271 ignoring function definition");
+      }
       name[char_index] = '=';
       /* temp_var can be NULL if it was an exported function with a syntax
 	 error (a different bug, but it still shouldn't dump core). */
@@ -3017,42 +3022,21 @@ merge_temporary_env ()
 /* **************************************************************** */
 
 static inline char *
-mk_env_string (name, value, isfunc)
+mk_env_string (name, value, is_function)
      const char *name, *value;
-     int isfunc;
+     int is_function;
 {
-  size_t name_len, value_len;
-  char	*p, *q;
+  int name_len, value_len, total_len;
+  char	*p;
 
-  name_len = strlen (name);
+  name_len = strlen (name) + (is_function ? ENV_FUNCTION_PREFIX_LEN + ENV_FUNCTION_SUFFIX_LEN : 0);
   value_len = STRLEN (value);
-
-  /* If we are exporting a shell function, construct the encoded function
-     name. */
-  if (isfunc && value)
-    {
-      p = (char *)xmalloc (BASHFUNC_PREFLEN + name_len + BASHFUNC_SUFFLEN + value_len + 2);
-      q = p;
-      memcpy (q, BASHFUNC_PREFIX, BASHFUNC_PREFLEN);
-      q += BASHFUNC_PREFLEN;
-      memcpy (q, name, name_len);
-      q += name_len;
-      memcpy (q, BASHFUNC_SUFFIX, BASHFUNC_SUFFLEN);
-      q += BASHFUNC_SUFFLEN;
-    }
-  else
-    {
-      p = (char *)xmalloc (2 + name_len + value_len);
-      memcpy (p, name, name_len);
-      q = p + name_len;
-    }
-
-  q[0] = '=';
+  total_len = 2 + name_len + value_len;
+  p = (char *)xmalloc (total_len);
+  snprintf(p, total_len, "%s%s%s=", is_function ? ENV_FUNCTION_PREFIX : "",
+           name, is_function ? ENV_FUNCTION_SUFFIX : "");
   if (value && *value)
-    memcpy (q + 1, value, value_len + 1);
-  else
-    q[1] = '\0';
-
+    strlcat(p, value, total_len);
   return (p);
 }
 
@@ -3127,7 +3111,7 @@ make_env_array_from_var_list (vars)
 	  /* Gee, I'd like to get away with not using savestring() if we're
 	     using the cached exportstr... */
 	  list[list_index] = USE_EXPORTSTR ? savestring (value)
-					   : mk_env_string (var->name, value, function_p (var));
+					   : mk_env_string (var->name, value, function_p(var));
 
 	  if (USE_EXPORTSTR == 0)
 	    SAVE_EXPORTSTR (var, list[list_index]);
